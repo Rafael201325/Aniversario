9acda8346abba564c0c40bedbaddef24d585b226
Rafael Siqueira <83845453+Rafael201325@users.noreply.github.com>
2026-08-06 19:24:11 -0400

    chore: anonimizar dados pessoais do convite público

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 // Edite somente este arquivo para personalizar o convite.
 window.INVITE_CONFIG = {
-  nome: "Sika",
-  data: "01 de agosto de 2026",
+  nome: "Convidado",
+  data: "10 de dezembro de 2026",
   horario: "19:00",
-  dataEvento: "2026-08-01T19:00:00-04:00",
-  local: "O de sempre",
-  googleMaps: "https://maps.app.goo.gl/YfeRcY26Dcs2SMbk8",
-  telefone: "5592994229534",
+  dataEvento: "2026-12-10T19:00:00-04:00",
+  local: "Local do evento",
+  googleMaps: "https://maps.google.com/",
+  telefone: "5592999999999",
   mensagemWhatsApp: "Missão aceita! Confirmo minha presença.",
   traje: "Esporte casual",
   observacoes: "Leve sua melhor energia. Esta mensagem é confidencial.",
   corPrincipal: "#71ff9b",
   imagemFundo: "",
   logo: "assets/icons/icon.svg",
-  musica: "assets/audio/musica.mp3"
+  musica: ""
 };
